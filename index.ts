126fee6ce7de2704badaa55d8b14bbfdd5b5a88f
khanzele16 <khanzele16@gmail.com>
2025-08-08 18:37:53 +0300

    First Commit

---
+++ index.ts
@@ -0,0 +1,26 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.post("/prodamus", async (req, res) => {
+  const paymentData = req.body;
+
+  console.log("Пришло уведомление от Prodamus:", paymentData);
+
+  //   if (paymentData.status === 'paid') {
+  //     await fetch(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`, {
+  //       method: 'POST',
+  //       headers: { 'Content-Type': 'application/json' },
+  //       body: JSON.stringify({
+  //         chat_id: CHAT_ID,
+  //         text: `✅ Оплата получена от ${paymentData.customer_email || 'неизвестного'}`
+  //       })
+  //     });
+  //   }
+
+  res.sendStatus(200);
+});
+
+app.listen(3000, () => console.log("Webhook-сервер запущен"));
