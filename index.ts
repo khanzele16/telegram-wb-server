6916507456bf54d703105e28c811e4f7b02fec64
khanzele <157638579+khanzele16@users.noreply.github.com>
2025-08-09 01:42:08 +0300

    Sa

--- index.ts
+++ index.ts
@@ -18,31 +18,31 @@ app.use((req, res, next) => {
 });
 
 app.post("/prodamus", async (req, res) => {
+  console.log(`${process.env.BOT_TOKEN}`)
   console.log(req.body)
   // if (req.body[0].payment_status === "success") {
-    // await fetch(
-    //   `https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`,
-    //   {
-    //     method: "POST",
-    //     headers: { "Content-Type": "application/json" },
-    //     body: JSON.stringify({
-    //       chat_id: req.body.order_id,
-    //       text: `✅ <b>Оплата получена!</b>\nКод заказа: <code>${req.body.order_id}</code>\n\nНажмите кнопку ниже, чтобы вступить в канал.`,
-    //       parse_mode: "HTML",
-    //       reply_markup: {
-    //         inline_keyboard: [
-    //           [
-    //             {
-    //               text: "🚀 Вступить в канал",
-    //               url: `${process.env.CHANNEL_LINK}`,
-    //             },
-    //           ],
-    //         ],
-    //       },
-    //     }),
-    //   }
-    // );
-  //   console.log(`${process.env.BOT_TOKEN}`)
+  // await fetch(
+  //   `https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`,
+  //   {
+  //     method: "POST",
+  //     headers: { "Content-Type": "application/json" },
+  //     body: JSON.stringify({
+  //       chat_id: req.body.order_id,
+  //       text: `✅ <b>Оплата получена!</b>\nКод заказа: <code>${req.body.order_id}</code>\n\nНажмите кнопку ниже, чтобы вступить в канал.`,
+  //       parse_mode: "HTML",
+  //       reply_markup: {
+  //         inline_keyboard: [
+  //           [
+  //             {
+  //               text: "🚀 Вступить в канал",
+  //               url: `${process.env.CHANNEL_LINK}`,
+  //             },
+  //           ],
+  //         ],
+  //       },
+  //     }),
+  //   }
+  // );
   // }
 
   res.sendStatus(200);
